8b510d63908458dfab949036789d26654eb80ee2
Alexander Senier <mail@senier.net>
2024-01-25 22:30:07 +0100

    Test main

---
+++ tests/unit/test_main.py
@@ -0,0 +1,27 @@
+import sys
+
+import pytest
+from click import Path
+
+from cobrafuzz.main import CobraFuzz
+
+
+def test_main(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> None:
+    def target(_: bytes) -> None:
+        pass
+
+    with monkeypatch.context() as mp:
+        mp.setattr(
+            sys,
+            "argv",
+            [
+                "main",
+                "--crash-dir",
+                str(tmp_path),
+                "--runs",
+                "100",
+            ],
+        )
+        c = CobraFuzz(target)
+        with pytest.raises(SystemExit, match="^0$"):
+            c()
