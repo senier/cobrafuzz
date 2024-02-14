a99189955d9ccca7e9acf4c95d4630d27d904d80
Alexander Senier <senier@adacore.com>
2024-02-14 22:19:13 +0100

    Add codeop seeds

---
+++ examples/fuzz_codeop/README.md
@@ -0,0 +1,2 @@
+Seeds source:
+    - https://rosettacode.org/wiki/Rosetta_Code/Count_examples/Full_list
---
+++ examples/fuzz_codeop/seeds/conditional.py
@@ -0,0 +1,27 @@
+x = int(input())
+
+
+def foo():
+    pass
+
+
+def bar():
+    pass
+
+
+def baz():
+    pass
+
+
+def boz():
+    pass
+
+
+if x == 0:
+    foo()
+elif x == 1:
+    bar()
+elif x == 2:
+    baz()
+else:
+    boz()
